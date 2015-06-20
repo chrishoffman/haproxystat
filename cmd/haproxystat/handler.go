c823995a8aebc3db7d7ac5fb0a9a6f4b9cb143ec
Chris Hoffman <christopher.hoffman@gmail.com>
2015-06-19 22:37:29 -0400

    Adding more HTTP stats

--- cmd/haproxystat/handler.go
+++ cmd/haproxystat/handler.go
@@ -32,12 +32,35 @@ func (s *statsdHandler) logHandler() func(*haproxy.Log) {
 }
 
 func (s *statsdHandler) sendHTTPStats(log *haproxy.Log) {
-	// Request stats
-	requestStatPrefix := fmt.Sprintf("%s.%s.%s", cleanStatToken(log.FrontendName),
-		cleanStatToken(log.BackendName), cleanAndLowerStatToken(log.ServerName))
+	frontendName := cleanStatToken(log.BackendName)
+	backendName := cleanStatToken(log.FrontendName)
+	serverName := cleanAndLowerStatToken(log.ServerName)
+	requestStatPrefix := fmt.Sprintf("%s.%s.%s", frontendName, backendName, serverName)
 
-	// HTTP Status Codes
-	s.client.Incr(fmt.Sprintf("%s.http_status.%d", requestStatPrefix, log.HTTPStatusCode), 1)
+	// Request stats
+	s.client.Incr(fmt.Sprintf("%s.response_size", requestStatPrefix), log.BytesRead)
+	s.client.Incr(fmt.Sprintf("%s.hits", requestStatPrefix), 1)
+	s.client.Incr(fmt.Sprintf("%s.responses.%d", requestStatPrefix, log.HTTPStatusCode), 1)
+
+	// Timing Stats
+	s.timing(fmt.Sprintf("%s.response_time", requestStatPrefix), log.Tt)
+	s.timing(fmt.Sprintf("%s.queue_time", requestStatPrefix), log.Tw)
+	s.timing(fmt.Sprintf("%s.request_time", requestStatPrefix), log.Tq)
+
+	// Misc Stats (stored in timing to average)
+	s.timing(fmt.Sprintf("%s.retries", requestStatPrefix), log.Retries)
+	s.timing(fmt.Sprintf("%s.queue", requestStatPrefix), log.ServerQueue)
+	s.timing(fmt.Sprintf("%s.active_connections", requestStatPrefix), log.ActConn)
+	s.timing(fmt.Sprintf("%s.backend_connections", requestStatPrefix), log.BeConn)
+	s.timing(fmt.Sprintf("%s.frontend_connections", requestStatPrefix), log.FeConn)
+	s.timing(fmt.Sprintf("%s.server_connections", requestStatPrefix), log.SrvConn)
+	s.timing(fmt.Sprintf("%s.response_size", requestStatPrefix), log.BytesRead)
+
+	// Backend Stats
+	backendPrefix := fmt.Sprintf("backend.%s", backendName)
+	s.timing(fmt.Sprintf("%s.connect_time", backendPrefix), log.Tc)
+	s.timing(fmt.Sprintf("%s.response_time", backendPrefix), log.Tr)
+	s.timing(fmt.Sprintf("%s.queue", backendPrefix), log.BackendQueue)
 
 	// Endpoint stats
 	pathParts := strings.Split(log.HTTPRequest.URL.Path, "/")
@@ -46,7 +69,7 @@ func (s *statsdHandler) sendHTTPStats(log *haproxy.Log) {
 		if basePath == "" {
 			basePath = "_root_"
 		}
-		s.client.Timing(fmt.Sprintf("%s.endpoint.%s.%s", requestStatPrefix, cleanAndLowerStatToken(basePath),
+		s.timing(fmt.Sprintf("%s.endpoint.%s.%s.response_time", requestStatPrefix, cleanAndLowerStatToken(basePath),
 			log.HTTPRequest.Method), log.Tt)
 	}
 
@@ -56,6 +79,13 @@ func (s *statsdHandler) sendHTTPStats(log *haproxy.Log) {
 			cleanStatToken(log.SslVersion), cleanStatToken(log.SslCipher))
 		s.client.Incr(sslStat, 1)
 	}
+}
+
+func (s *statsdHandler) timing(stat string, delta int64) {
+	if delta == -1 {
+		return
+	}
+	s.client.Timing(stat, delta)
 }
 
 func cleanAndLowerStatToken(s string) string {
